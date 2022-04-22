d4ce350eab1e867817e25c20b3e4b4fdf86e9f00
thiengoten <thiengoten6@gmail.com>
2022-04-22 20:49:49 +0700

    first commit

---
+++ week5/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using week5.Areas.Identity.Data;
+
+namespace week5.Models
+{
+    public class Order
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public double Total { get; set; }
+        public AppUser User { get; set; }
+        public virtual ICollection<OrderDetail>? OrderDetails { get; set; }
+
+    }
+}
---
+++ week5/Models/Store.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using week5.Areas.Identity.Data;
+
+namespace week5.Models
+{
+    public class Store
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Slogan { get; set; }
+        public string UserId { get; set; }
+        public AppUser User { get; set; }
+        public virtual ICollection<Book>? Books { get; set; }
+
+    }
+}
